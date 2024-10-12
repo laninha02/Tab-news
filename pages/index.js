640573352ab542e5a9230aff350db8823aad3bdb
Elaine Namura <elainenamura@Carloss-MacBook-Air.local>
2024-10-12 18:03:50 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>Bleach é melhor que One Piece e voces não estão preparados pra isso!</h1>
+    return <h1>Bleach é melhor que One Piece e voces não estão preparados pra isso!!</h1>
 }
 
 export default Home;
+git 
