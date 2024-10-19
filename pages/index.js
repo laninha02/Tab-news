92966eed4433d012002526545fcf152a64f4d7c2
Elaine Namura <elainenamura@Carloss-MacBook-Air.local>
2024-10-19 16:53:02 -0300

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Bleach é melhor que One Piece e voces não estão preparados pra isso!!</h1>
+function Home() {
+  return (
+    <h1>
+      Bleach é melhor que One Piece e voces não estão preparados pra isso!!
+    </h1>
+  );
 }
 
 export default Home;
